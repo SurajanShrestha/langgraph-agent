dbe821c34aa4889ec443a868e4b0facc489361af
SurajanShrestha <surajanshrestha35@gmail.com>
2025-06-12 09:59:10 +0545

    feat: state graph and workflow added

--- agent.ts
+++ agent.ts
@@ -1,44 +1,69 @@
-import { MemorySaver } from "@langchain/langgraph";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { MessagesAnnotation, START, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { ChatOpenAI } from "@langchain/openai";
 import { TavilySearch } from "@langchain/tavily";
-import { v4 as uuidv4 } from "uuid";
 import "dotenv/config";
+import { AIMessage } from "@langchain/core/messages";
+
+// NOTE 💡: INDUSTRY STANDARD WAY OF CREATING AGENT WITH FINE GRAINED CONTROL USING STATE GRAPH
 
 // Initialize the TavilySearch tool with a maximum of 3 results
 const agentTools = [new TavilySearch({ maxResults: 3 })];
+const toolNode = new ToolNode(agentTools);
 
 // Initialize the OpenAI model with the specified parameters
 const agentModel = new ChatOpenAI({
   model: "gpt-4.1-nano",
   temperature: 0,
   openAIApiKey: process.env.OPENAI_API_KEY,
-});
+}).bindTools(agentTools);
 
-// Create a memory saver to store the agent's state
-const memory = new MemorySaver();
-
-// Create the agent with the model, tools, and memory saver
-const agent = await createReactAgent({
-  llm: agentModel,
-  tools: agentTools,
-  checkpointSaver: memory,
-});
-
-// Create a unique thread ID for identifying that conversation
-const config = { configurable: { thread_id: uuidv4() } };
-
-// First interaction with the agent
-const input = {
-  role: "user",
-  content: "What is the current weather in kathmandu?",
+// Define the function that determines the whether to continue or not
+const shouldContinue = ({ messages }: typeof MessagesAnnotation.State) => {
+  const lastMessage = messages[messages.length - 1] as AIMessage;
+  // If the LLM makes a tool call, then we route to the "tools" node
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+  // Otherwise, we stop (reply to the user) using the special "__end__" node
+  return "__end__";
 };
-const output = await agent.invoke({ messages: input }, config);
 
-console.log(output?.messages?.[output.messages.length - 1]?.content);
+// Define the function that calls the model
+const callModel = async (state: typeof MessagesAnnotation.State) => {
+  const response = await agentModel.invoke(state.messages);
+  // We return a list, because this will get added to the existing list
+  return { messages: [response] };
+};
 
-// Second interaction with the agent, using the same thread ID
-const input2 = { role: "user", content: "What is about biratnagar?" };
-const output2 = await agent.invoke({ messages: input2 }, config);
+// Define a new graph
+// START --> agent  <-- tools
+// agent --> shouldContinue (YES) --> tools
+// agent --> shouldContinue (NO) --> END
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge(START, "agent")
+  .addNode("tools", toolNode)
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
 
-console.log(output2?.messages?.[output2.messages.length - 1]?.content);
+// Compile it to a a Langchain Runnable
+const app = workflow.compile();
+
+// Use the agent
+const finalState = await app.invoke({
+  messages: { role: "user", content: "What is the weather in Kathmandu?" },
+});
+console.log(finalState?.messages?.[finalState.messages.length - 1]?.content);
+
+// IMP 💡: WE SHOULD'VE USED "MemorySaver()" INSTEAD OF APPENDING PREVIOUS MESSAGES.
+// HERE, WE APPEND BECAUSE THIS LESSON TEACHES THE RAW MECHANICS OF LANGGRAPH
+// BUT YES, IT SACRIFICES DEVELOPER ERGONOMICS FOR CLARITY
+// IF BUILDING A REAL AGENT, USE "MemorySaver()" TO STORE THE STATE (DONE IN chatbot-llm project)
+const nextState = await app.invoke({
+  messages: [
+    ...finalState?.messages,
+    { role: "user", content: "What about Biratnagar?" },
+  ],
+});
+console.log(nextState?.messages?.[nextState.messages.length - 1]?.content);
